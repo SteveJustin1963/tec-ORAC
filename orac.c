ec871839500eba149bde1b6a6f79a45ae516a8aa
Stephen Justin <stevejustin1963@gmail.com>
2022-05-03 15:44:53 +1000

    Update orac.c

--- orac.c
+++ orac.c
@@ -1,1 +1,8 @@
-\\ 
+ 
+// Orac is a computer and its abilities are listed as: 
+// 1.  Access other computers data and or remote control them 
+// 2. This signals are sent over wireless signals
+// 3. has an AI personality that learns from its users and has curiosity about the universe 
+// 4.  can simulate or decode different signals
+// 5. to predict probability outcomes give input parameters 
+// 6. has hacking, intrusion and virus protection software
